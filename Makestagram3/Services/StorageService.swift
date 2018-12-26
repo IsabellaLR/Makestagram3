a2557e52bc9bea54ac56f9bc37295c4432b7a6fd
IsabellaLR <isabellaleerubio21@gmail.com>
2018-12-25 22:20:14 -0800

    Loading Photos to Firebase

---
+++ Makestagram3/Services/StorageService.swift
@@ -0,0 +1,39 @@
+//
+//  StorageService.swift
+//  Makestagram3
+//
+//  Created by Bella on 12/25/18.
+//  Copyright © 2018 Bella. All rights reserved.
+//
+
+import UIKit
+import FirebaseStorage
+
+struct StorageService {
+    // provide method for uploading images
+    static func uploadImage(_ image: UIImage, at reference: StorageReference, completion: @escaping (URL?) -> Void) {
+        // 1
+        guard let imageData = image.jpegData(compressionQuality: 0.1) else {
+            return completion(nil)
+        }
+        
+        // 2
+        reference.putData(imageData, metadata: nil, completion: { (metadata, error) in
+            // 3
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return completion(nil)
+            }
+            
+            // 4
+            reference.downloadURL(completion: { (url, error) in
+                if let error = error {
+                    assertionFailure(error.localizedDescription)
+                    return completion(nil)
+                }
+                completion(url)
+            })
+        })
+    }
+}
+
