916da795daddeb396f0818a3587f18ea63106637
IsabellaLR <isabellaleerubio21@gmail.com>
2018-12-27 21:33:28 -0800

    Changing HomeVC basic layout

--- Makestagram3/Controllers/HomeViewController.swift
+++ Makestagram3/Controllers/HomeViewController.swift
@@ -9,5 +9,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        UserService.posts(for: User.current) { (posts) in
+            self.posts = posts
+            self.tableView.reloadData()
+        }
+    }
+    
+    var posts = [Post]()
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
+        cell.backgroundColor = .red
+        
+        return cell
+    }
+    
 }
