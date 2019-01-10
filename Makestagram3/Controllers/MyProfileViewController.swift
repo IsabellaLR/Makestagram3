d752365ae92eedb022af6acae0503074cf1384b2
IsabellaLR <isabellaleerubio21@gmail.com>
2019-01-09 22:36:13 -0800

    Set Up Avatar

--- Makestagram3/Controllers/MyProfileViewController.swift
+++ Makestagram3/Controllers/MyProfileViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
 class MyProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var profileButton: UIButton!
@@ -14,40 +16,123 @@ class MyProfileViewController: UIViewController, UIImagePickerControllerDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        profileButton.frame = CGRect(x: 160, y: 130, width: 100, height: 100)
+        profileButton.layer.cornerRadius = 0.5 * profileButton.bounds.size.width
+        profileButton.clipsToBounds = true
+//        profileButton.layer.borderWidth = 1
+//        profileButton.layer.borderColor = UIColor.black.cgColor
+//        self.applyRoundCorner(profileButton)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+//    func applyRoundCorner(_ object: AnyObject) {
+//        object.layer.cornerRadius = object.frame.size.width / 2
+//        object.layer.masksToBounds = true
+//    }
+    
     @IBAction func addPhoto(_ sender: UIButton) {
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         imagePicker = UIImagePickerController()
         imagePicker.delegate = self
         
-        let cameraAction = UIAlertAction(title: "Use Camera", style: .default) { (action) in
-            self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
-            self.imagePicker.sourceType = .camera
-            self.imagePicker.allowsEditing = true
-            
-            self.present(self.imagePicker, animated: true, completion: nil)
+        if (UIImagePickerController.isSourceTypeAvailable(.camera))
+        {
+            let cameraAction = UIAlertAction(title: "Use Camera", style: .default) { (action) in
+                
+                let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+                
+                if (status == .authorized){
+                    self.DisplayPicker(type: .camera)
+                }
+                if (status == .restricted){
+                    self.HandleRestricted()
+                }
+                if (status == .denied){
+                    self.HandleDenied()
+                }
+                if (status == .notDetermined){
+                    AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { (granted) in
+                        if (granted){
+                            self.DisplayPicker(type: .camera)
+                        }
+                    })
+                }
+            }
+             alertController.addAction(cameraAction)
         }
         
-        let photoLibraryAction = UIAlertAction(title: "Use PhotoLibrary", style: .default) { (action) in
-            self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
-            self.imagePicker.sourceType = .photoLibrary
-            self.imagePicker.allowsEditing = true
-            
-            self.present(self.imagePicker, animated: true, completion: nil)
+        if (UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            let photoLibraryAction = UIAlertAction(title: "Use Photo Library", style: .default) { (action) in
+
+                let status = PHPhotoLibrary.authorizationStatus()
+                
+                if (status == .authorized){
+                    self.DisplayPicker(type: .photoLibrary)
+                }
+                if (status == .restricted){
+                    self.HandleRestricted()
+                }
+                if (status == .denied){
+                    self.HandleDenied()
+                }
+                if (status == .notDetermined){
+                    PHPhotoLibrary.requestAuthorization({ (status) in
+                        if (status == PHAuthorizationStatus.authorized){
+                            self.DisplayPicker(type: .photoLibrary)
+                        }
+                    })
+                }
+            }
+            alertController.addAction(photoLibraryAction)
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
-        alertController.addAction(cameraAction)
-        alertController.addAction(photoLibraryAction)
         alertController.addAction(cancelAction)
         
+        alertController.popoverPresentationController?.sourceView = self.profileButton
+        alertController.popoverPresentationController?.sourceRect = self.profileButton.bounds
+        
         present(alertController, animated: true, completion: nil)
+    }
+    
+    func HandleDenied(){
+        let alertController = UIAlertController(title: "Media Access Denied", message: "CameraTutorial does not access to your device's media. Please update your settings", preferredStyle: .alert)
+        let settingsAction = UIAlertAction(title: "Go to Settings", style: .default){ (action) in
+            DispatchQueue.main.async {
+                UIApplication.shared.open(NSURL(string: UIApplication.openSettingsURLString)! as URL)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.popoverPresentationController?.sourceView = self.profileButton
+        alertController.popoverPresentationController?.sourceRect = self.profileButton.bounds
+        
+        alertController.addAction(settingsAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func HandleRestricted(){
+        let alertController = UIAlertController(title: "Media Access Denied", message: "This device is restricited from accessing any media at this time", preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alertController.popoverPresentationController?.sourceView = self.profileButton
+        alertController.popoverPresentationController?.sourceRect = self.profileButton.bounds
+        
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func DisplayPicker(type: UIImagePickerController.SourceType){
+        self.imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: type)!
+        self.imagePicker.sourceType = type
+        self.imagePicker.allowsEditing = true
+        
+        DispatchQueue.main.async {
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
