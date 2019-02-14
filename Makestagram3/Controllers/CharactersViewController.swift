d59a3616d944ca452a2568c066181e22c08e4ce4
IsabellaLR <isabellaleerubio21@gmail.com>
2019-02-13 23:18:46 -0800

    Change Collection View Format

--- Makestagram3/Controllers/CharactersViewController.swift
+++ Makestagram3/Controllers/CharactersViewController.swift
@@ -6,102 +6,77 @@
 //  Copyright © 2019 Bella. All rights reserved.
 //
 
-import Foundation
 import UIKit
 
-class CharactersViewController: UIViewController {
+/// same with UITableViewCell's selected backgroundColor
+//private let highlightedColor = UIColor(rgb: 0xD8D8D8)
+
+class CharactersViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    //UI for CollectionView
+    let characters = ["Jaime Lannister", "Cersei Lannister", "Daenerys Targaryen",  "Jon Snow", "Sansa Stark", "Arya Stark", "Theon Greyjoy", "Bran Stark", "The Hound", "Tyrion Lannister", "Davos Seaworth", "Samwell Tarly", "Melisandre", "Bronn", "Varys", "Gendry", "Brienne of Tarth", "Gilly", "Daario Naharis", "Missandei", "Jaqen H'ghar", "Podrick Payne", "Yara Greyjoy", "Grey Worm", "Meera Reed", "Ghost"]
+    let characterImages = ["ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1"]
     
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    //    @IBOutlet weak var navigationTitle: UINavigationItem!
-    
-    var characters = ["Jaime Lannister", "Cersei Lannister", "Daenerys Targaryen",  "Jon Snow", "Sansa Stark", "Arya Stark", "Theon Greyjoy", "Bran Stark", "The Hound", "Tyrion Lannister", "Davos Seaworth", "Samwell Tarly", "Melisandre", "Bronn", "Varys", "Gendry", "Brienne of Tarth", "Gilly", "Daario Naharis", "Missandei", "Jaqen H'ghar", "Podrick Payne", "Yara Greyjoy", "Grey Worm", "Meera Reed", "Ghost"]
-    var characterImages = ["ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1", "ep1"]
-    
-    var estimateWidth = 160.0
-    var cellMarginSize = 16.0
+//    var estimateWidth = 160.0
+//    var cellMarginSize = 16.0
+//
+//    var shouldTintBackgroundWhenSelected = true // You can change default value
+//    var specialHighlightedArea: UIView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // navigationTitle.title = topic.topicType
-        
-        // Set Delegates
-        self.collectionView.delegate = self
-        self.collectionView.dataSource = self
-        
-        collectionView.allowsMultipleSelection = true
-        
-        // Register cells
-        self.collectionView.register(UINib(nibName: "ThroneCell", bundle: nil), forCellWithReuseIdentifier: "ThroneCell")
-        
-        // SetupGrid view
-        self.setupGridView()
     }
     
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        self.setupGridView()
-        DispatchQueue.main.async {
-            self.collectionView.reloadData()
-        }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    
-    func setupGridView() {
-        let flow = collectionView?.collectionViewLayout as! UICollectionViewFlowLayout
-        flow.minimumInteritemSpacing = CGFloat(self.cellMarginSize)
-        flow.minimumLineSpacing = CGFloat(self.cellMarginSize)
-    }
-}
-
-extension CharactersViewController: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.characters.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ThroneCell", for: indexPath) as! ThroneCell
-        cell.setData(text: self.characters[indexPath.row])
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "throneCell", for: indexPath) as! CharacterViewCell
+        cell.characterName.text = characters[indexPath.row]
+//        cell.isUserInteractionEnabled = true
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ThroneCell", for: indexPath) as! ThroneCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "throneCell", for: indexPath) as! CharacterViewCell
         let character = characters[indexPath.row]
-        
+        cell.backgroundColor = UIColor.red
         onBoardingService.pickCharacter(character: character)
-        cell.layer.borderWidth = 1.0
-        cell.layer.borderColor = UIColor.gray.cgColor
-//        self.collectionView.reloadItems(at: [indexPath])
+        collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ThroneCell", for: indexPath) as! ThroneCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "throneCell", for: indexPath) as! CharacterViewCell
         let character = characters[indexPath.row]
-        
+        cell.backgroundColor = UIColor.clear
         onBoardingService.removeCharacter(character: character)
-        cell.layer.borderWidth = 5.0
-        cell.layer.borderColor = UIColor.red.cgColor
-//        self.collectionView.reloadItems(at: [indexPath])
+        collectionView.reloadData()
     }
 }
 
-extension CharactersViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = self.calculateWith()
-        return CGSize(width: width, height: width)
-    }
-    
-    func calculateWith() -> CGFloat {
-        let estimatedWidth = CGFloat(estimateWidth)
-        let cellCount = floor(CGFloat(self.view.frame.size.width / estimatedWidth))
-        
-        let margin = CGFloat(cellMarginSize * 2)
-        let width = (self.view.frame.size.width - CGFloat(cellMarginSize) * (cellCount - 1) - margin) / cellCount
-        
-        return width
-    }
-}
+//extension CharactersViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = self.calculateWith()
+//        return CGSize(width: width, height: width)
+//    }
+//
+//    func calculateWith() -> CGFloat {
+//        let estimatedWidth = CGFloat(estimateWidth)
+//        let cellCount = floor(CGFloat(self.view.frame.size.width / estimatedWidth))
+//
+//        let margin = CGFloat(cellMarginSize * 2)
+//        let width = (self.view.frame.size.width - CGFloat(cellMarginSize) * (cellCount - 1) - margin) / cellCount
+//
+//        return width
+//    }
+//}
+
+//extension UIColor {
+//    convenience init(rgb: Int, alpha: CGFloat = 1.0) {
+//        self.init(red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0, green: CGFloat((rgb & 0xFF00) >> 8) / 255.0, blue: CGFloat(rgb & 0xFF) / 255.0, alpha: alpha)
+//    }
+//}
---
+++ Makestagram3/Views/CharacterViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterViewCell.swift
+//  Makestagram3
+//
+//  Created by Bella on 2/13/19.
+//  Copyright © 2019 Bella. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var characterName: UILabel!
+}
