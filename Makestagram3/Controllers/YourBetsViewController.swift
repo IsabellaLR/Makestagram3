12a58a0f4999502b3054076a81aa6b3ecd1c62b8
IsabellaLR <isabellaleerubio21@gmail.com>
2019-03-01 01:36:22 -0800

    Page Bet View Controllers

---
+++ Makestagram3/Controllers/YourBetsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  YourBetsViewController.swift
+//  Makestagram3
+//
+//  Created by Bella on 3/1/19.
+//  Copyright © 2019 Bella. All rights reserved.
+//
+
+import UIKit
+
+class YourBetsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
